aa5272f7d307f576923381bd8a3edc15845a5164
Muhammad Usman Khilji <manian1221@gmail.com>
2024-10-24 17:35:35 +0500

    Added user resource, postgres connection

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,14 +8,33 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
+const config_1 = require("@nestjs/config");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const auth_module_1 = require("./auth/auth.module");
+const user_module_1 = require("./user/user.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            config_1.ConfigModule.forRoot(),
+            auth_module_1.AuthModule,
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'postgres',
+                host: process.env.DB_HOST,
+                port: parseInt(process.env.DB_PORT),
+                password: process.env.DB_PASSWORD,
+                username: process.env.DB_USERNAME,
+                entities: [],
+                database: process.env.DB,
+                synchronize: true,
+                logging: true,
+            }),
+            user_module_1.UserModule,
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      entities: [],
+      database: process.env.DB,
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
